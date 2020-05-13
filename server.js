3a9b966464a2e4e523b7df5428fbd5b99289777e
Jake Mulley <jakemulley@users.noreply.github.com>
2020-05-13 09:48:06 +0100

    Change exposed URL for assets and this repository

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const port = process.env.PORT || 8080
 
-app.use(express.static(path.join(__dirname, 'docs')))
-app.listen(8080)
+app.use('/content-frontend', express.static(path.join(__dirname, 'docs')))
+app.listen(port)
+console.log('content-frontend has been built to http://localhost:' + port + '/content-frontend')
